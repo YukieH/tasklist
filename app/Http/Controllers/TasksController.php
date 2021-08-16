f3f8c8a7928be433206777dc69730fdd790bd82c
Yukie.Hayakawa <ubuntu@ip-172-31-12-18.ap-northeast-1.compute.internal>
2021-08-16 12:33:31 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -37,11 +37,13 @@ class TasksController extends Controller
         
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:255',
         ]);
         
         // メッセージを作成
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -79,12 +81,13 @@ class TasksController extends Controller
         
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:255',
         ]);
         
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
-        // メッセージを更新
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
